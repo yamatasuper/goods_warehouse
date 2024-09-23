e78881efc6e084d3142308a9a71c956c3419e536
Артем Мовчан <mob.ar@mail.ru>
2024-09-23 22:05:02 +0400

    Ktor project generator

---
+++ src/main/kotlin/com/goods/warehouse/Application.kt
@@ -0,0 +1,13 @@
+package com.goods.warehouse
+
+import com.goods.warehouse.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureRouting()
+}
---
+++ src/main/kotlin/com/goods/warehouse/plugins/Serialization.kt
@@ -0,0 +1,9 @@
+package com.goods.warehouse.plugins
+
+import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+
+fun Application.configureSerialization() {
+    install(ContentNegotiation) {
+    }
+}
