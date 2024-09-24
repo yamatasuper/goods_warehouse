0158c2bd662fc202f748cf8ba82f44cee736db61
Артем Мовчан <mob.ar@mail.ru>
2024-09-24 23:21:11 +0400

    Таблица для хранения товаров

--- sql/init-db.sql
+++ sql/init-db.sql
@@ -1,5 +1,13 @@
-create table goods
+CREATE TABLE goods
 (
-    id   varchar(80) primary key,
-    name varchar(80) not null,
+    id                      SERIAL PRIMARY KEY,  -- Уникальный идентификатор (или можно использовать UUID)
+    name                    VARCHAR(255) NOT NULL,  -- Название товара
+    sku                     VARCHAR(100) UNIQUE NOT NULL,  -- Артикул (уникальный)
+    description             TEXT,  -- Описание товара
+    category                VARCHAR(100),  -- Категория товара
+    price                   DECIMAL(10, 2) NOT NULL,  -- Цена товара
+    quantity                INTEGER NOT NULL,  -- Количество на складе
+    last_quantity_update    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,  -- Дата и время последнего изменения количества
+    created_at              TIMESTAMP DEFAULT CURRENT_TIMESTAMP  -- Дата создания записи
 );
+
