fc6f327d876179eccf3b8bda1405ba640e26ee74
movchanAS <mob.ar@mail.ru>
2024-11-19 12:24:54 +0300

    локальный старт приложения под отдельным профилем.

--- src/main/kotlin/com/goods/product/Application.kt
+++ src/main/kotlin/com/goods/product/Application.kt
@@ -10,36 +10,58 @@ import io.ktor.server.netty.Netty
 import org.jetbrains.exposed.sql.Database
 
 fun main() {
-    Database.connect(
-        url = System.getenv("DATABASE_CONNECTION_STRING"),
-        driver = "org.postgresql.Driver",
-        user = System.getenv("POSTGRES_USER"),
-        password = System.getenv("POSTGRES_PASSWORD")
-    )
+    // Подключение к базе данных
+    val environment = System.getenv("KTOR_ENV") ?: "local"
+    if (environment == "local") {
+        Database.connect(
+            url = System.getenv("DATABASE_CONNECTION_STRING"),
+            driver = "org.postgresql.Driver",
+            user = System.getenv("POSTGRES_USER"),
+            password = System.getenv("POSTGRES_PASSWORD")
+        )
+    } else {
+        Database.connect(
+            url = System.getenv("DATABASE_CONNECTION_STRING"),
+            driver = "org.postgresql.Driver",
+            user = System.getenv("POSTGRES_USER"),
+            password = System.getenv("POSTGRES_PASSWORD")
+        )
+    }
 
+    // Запуск сервера
+    val port = System.getenv("SERVER_PORT")?.toIntOrNull() ?: 8080
     embeddedServer(
         Netty,
-        port = System.getenv("SERVER_PORT").toInt(),
-        module = Application::goodsWarehouse
+        port = port,
+        module = Application::module // Вызывает метод module для настройки окружения
     ).start(wait = true)
-
-//TODO: убрать локальный запуск
-//    Database.connect(
-//        url = "jdbc:postgresql://localhost:5432/artemmovchan",
-//        driver = "org.postgresql.Driver",
-//        user = "postgres",
-//    )
-//
-//    embeddedServer(
-//        Netty,
-//        port = 8080,
-//        module = Application::goodsWarehouse
-//    ).start(wait = true)
 }
-fun Application.goodsWarehouse() {
+
+fun Application.module() {
+    val environmentType = environment.config.propertyOrNull("ktor.deployment.environment")?.getString() ?: "local"
+    log.info("Running in environment: $environmentType")
+
+    when (environmentType) {
+        "local" -> configureLocalFeatures()
+        "prod" -> configureProdFeatures()
+        else -> throw IllegalArgumentException("Unknown environment: $environmentType")
+    }
+}
+
+fun Application.configureLocalFeatures() {
+    log.info("Configuring local features")
     configureProductRouting()
     configureSerialization()
     configureSwagger()
     configureStatusPages()
 }
+
+fun Application.configureProdFeatures() {
+    log.info("Configuring production features")
+    configureProductRouting()
+    configureSerialization()
+    configureSwagger()
+    configureStatusPages()
+}
+
 
