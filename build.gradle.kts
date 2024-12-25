29a98fa2c7671415a5ac65e201b92fae934d5439
Артем Мовчан <mob.ar@mail.ru>
2024-12-25 16:08:08 +0400

    Запущенный проект springboot

--- build.gradle.kts
+++ build.gradle.kts
@@ -100,14 +100,19 @@
 //}
 
 plugins {
-    id("org.springframework.boot") version "3.0.0"
-    id("io.spring.dependency-management") version "1.1.0"
-    kotlin("jvm") version "1.9.22"
-    kotlin("plugin.spring") version "1.9.22"
+    java
+    id("org.springframework.boot") version "3.4.1"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
-group = "com.goods.product"
-version = "0.0.1"
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
 
 repositories {
     mavenCentral()
@@ -116,26 +121,31 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-logging")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-json")
-    implementation("org.postgresql:postgresql")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 
-    // Включение зависимости для Swagger
-    implementation("io.springfox:springfox-boot-starter:3.0.0")
+    // PostgreSQL driver
+    implementation("org.postgresql:postgresql")
 
-    // Для работы с Exposed
+    // Swagger/OpenAPI support
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
+
+    // Exposed ORM
     implementation("org.jetbrains.exposed:exposed-core:0.37.3")
     implementation("org.jetbrains.exposed:exposed-jdbc:0.37.3")
 
-    // Для bcrypt
+    // BCrypt for password hashing
     implementation("at.favre.lib:bcrypt:0.10.2")
 
-    // Тестирование
+    // H2 database (in-memory database for testing or development)
+    runtimeOnly("com.h2database:h2")
+
+    // Testing dependencies
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
 }
 
-
-
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
