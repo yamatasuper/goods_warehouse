a025ca92fcf3edb145b8d767f5a848f87b677c35
Артем Мовчан <mob.ar@mail.ru>
2025-02-10 19:19:01 +0400

    Поле isAvailable в таблице продуктов

--- src/main/java/com/goods/product/task1/service/DataGeneratorService.java
+++ src/main/java/com/goods/product/task1/service/DataGeneratorService.java
@@ -25,18 +25,14 @@ public class DataGeneratorService {
   public void generateBatch(int start, int batchSize, String currency) {
     List<Object[]> batchArgs = new ArrayList<>();
 
-    // Берем курс для выбранной валюты
     BigDecimal rate = exchangeRateService.getExchangeRate(currency);
-
-    // Если курс не найден, оставляем цену в RUB
     if (rate == null) {
       System.out.println("Курс для валюты " + currency + " не найден. Оставляем цену в RUB.");
-      rate = BigDecimal.valueOf(1); // Используем 1 как курс для RUB
+      rate = BigDecimal.valueOf(1);
     }
 
     for (int i = start; i < start + batchSize; i++) {
       BigDecimal priceInRub = BigDecimal.valueOf(100 + (i % 100));
-      // Переводим цену в нужную валюту
       BigDecimal convertedPrice = priceInRub.divide(rate, 2, RoundingMode.HALF_UP);
 
       batchArgs.add(
@@ -50,12 +46,14 @@ public class DataGeneratorService {
             "2025-01-14",
             "2025-01-01",
             currency,
-            0
+            0,
+            true // Новое поле is_available
           });
     }
 
     String sql =
-        "INSERT INTO product (name, sku, description, category, price, quantity, last_quantity_update, created_at, currency, version) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        "INSERT INTO product (name, sku, description, category, price, quantity, last_quantity_update, created_at, currency, version, is_available) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
     jdbcTemplate.batchUpdate(sql, batchArgs);
   }
 }
